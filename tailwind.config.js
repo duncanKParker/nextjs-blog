9ce96994cf64068020272beae69f8da010300dda
duncanKParker <31214804+duncanKParker@users.noreply.github.com>
2023-12-02 17:53:34 -0600

    Add changes for tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ['./pages/**/*.js', './components/**/*.js'],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
